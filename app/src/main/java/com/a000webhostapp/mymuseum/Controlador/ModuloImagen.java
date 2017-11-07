e2bd41010003ba498e08ea95be8aea9889beac11
Alexis <alexisfonzos_@hotmail.com>
2017-11-06 21:43:08 -0300

    En proceso de insertar imagenes

---
+++ app/src/main/java/com/a000webhostapp/mymuseum/Controlador/ModuloImagen.java
@@ -0,0 +1,166 @@
+package com.a000webhostapp.mymuseum.Controlador;
+
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
+
+import com.a000webhostapp.mymuseum.DAO.ControlFTP;
+import com.a000webhostapp.mymuseum.IObserver;
+import com.a000webhostapp.mymuseum.ISujeto;
+import com.a000webhostapp.mymuseum.Modelo.Guardable;
+import com.a000webhostapp.mymuseum.Modelo.Imagen;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Alexis on 5/11/2017.
+ */
+
+public class ModuloImagen implements ISujeto {
+	private ArrayList<IObserver> observers;
+	private ArrayList<Request> requests;
+	private static ModuloImagen mi;
+	
+	public static final int RQS_BUSQUEDA_IMAGEN_UNICA = 0;
+	
+	public static final int RQS_INSERTAR_IMAGEN = 100;
+	
+	private ModuloImagen(){
+		observers = new ArrayList<IObserver>();
+		requests = new ArrayList<Request>();
+	}
+	
+	public static ModuloImagen obtenerModulo(){
+		if(mi == null){
+			mi = new ModuloImagen();
+		}
+		return mi;
+	}
+	//----------------------
+	public void buscarImagen(String nombre, String entidad, IObserver observer){
+		RequestImagen ri = new RequestImagen(RQS_BUSQUEDA_IMAGEN_UNICA,nombre,entidad);
+		registrarObserver(observer,ri);
+		new ControlFTP(ri).buscar();
+	}
+	//----------------------
+	public void insertarImagen(String nombre, String entidad,Context context,Uri uri, IObserver observer){
+		RequestImagen ri = new RequestImagen(RQS_INSERTAR_IMAGEN, context,nombre,entidad);
+		registrarObserver(observer,ri);
+		new ControlFTP(ri).insertar(uri);
+	}
+	//----------------------
+	
+	//Metodos Observer
+	public void registrarObserver(IObserver ob, Request request) {
+		observers.add(ob);
+		requests.add(request);
+	}
+	
+	public boolean eliminarObserver(IObserver ob) {
+		requests.remove(observers.indexOf(ob));
+		observers.remove(ob);
+		return true;
+	}
+	
+	public void notificarObserver(Request request, Guardable[] g, String respuesta) {
+		for(int i = 0; i < requests.size(); i++){
+			if(request.equals(requests.get(i))){
+				observers.get(i).update(g,request.getId(),respuesta);
+			}
+		}
+	}
+	
+	public void resultadoControlFTP(Request request, String respuesta, Bitmap g){
+		switch (respuesta){
+			case "Exito":
+				switch (request.getId()){
+					case RQS_BUSQUEDA_IMAGEN_UNICA:
+						Imagen im = new Imagen(g);
+						Guardable[] gs = new Guardable[1];
+						gs[0] = im;
+						notificarObserver(request,gs,respuesta);
+						break;
+				}
+				break;
+		}
+	}
+	
+	
+	
+	/*protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.prueba);
+		
+		iv = (ImageView)findViewById(R.id.imageViewPRUEBA);
+		
+		Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+		intent.setType("image/*");
+		startActivityForResult(Intent.createChooser(intent,"Elegir imagen"),5);
+		
+		
+		r = new AsyncTask() {
+			protected Object doInBackground(Object[] objects) {
+				if (objects[0] instanceof Uri) {
+					Uri uri = (Uri) objects[0];
+					insertarImagen(uri);
+				}
+				return null;
+			}
+		};
+		
+		
+	}
+	private void insertarImagen(Uri uri){
+		boolean conecto = false;
+		try{
+			ftp = new FTPClient();
+			ftp.connect("files.000webhost.com",21);
+			if(FTPReply.isPositiveCompletion(ftp.getReplyCode())){
+				conecto = ftp.login("mymuseum", "39623299Aa");
+				ftp.setFileType(FTP.BINARY_FILE_TYPE);
+				ftp.enterLocalPassiveMode();
+			}
+			Log.v("Conectar",""+conecto);
+			
+			//InputStream is = getContentResolver().openInputStream(uri);
+			//boolean a = ftp.storeFile("/public_html/images/hola.jpeg", is);
+			//is.close();
+			
+			//Log.v("Guardar",""+a);
+			
+			final ByteArrayOutputStream os = new ByteArrayOutputStream();
+			
+			boolean a = ftp.retrieveFile("/public_html/images/hola.jpeg",os);
+			os.close();
+			
+			Log.v("Obtener",""+a);
+			
+			runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					Bitmap a = BitmapFactory.decodeByteArray(os.toByteArray(),0,os.toByteArray().length);
+					iv.setImageBitmap(a);
+				}
+			});
+			
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				ftp.logout();
+				ftp.disconnect();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(requestCode == 5){
+			r.execute(data.getData());
+		}
+	}*/
+	
+}
---
+++ app/src/main/java/com/a000webhostapp/mymuseum/DAO/ControlFTP.java
@@ -0,0 +1,129 @@
+package com.a000webhostapp.mymuseum.DAO;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.a000webhostapp.mymuseum.Controlador.ModuloImagen;
+import com.a000webhostapp.mymuseum.Controlador.Request;
+import com.a000webhostapp.mymuseum.Controlador.RequestImagen;
+import com.a000webhostapp.mymuseum.Modelo.Guardable;
+
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by Alexis on 6/11/2017.
+ */
+
+public class ControlFTP extends AsyncTask<Uri, String, Bitmap>{
+	private static String pathImagenFTP = "/public_html/images/";
+	private static String extensionArchivo = ".png";
+	
+	private Context context;
+	private FTPClient ftp;
+	private RequestImagen request;
+	public ControlFTP(RequestImagen request) {
+		this.request = request;
+	}
+	
+	public void buscar(){
+		execute();
+	}
+	public void insertar(Uri uri){
+		execute(uri);
+	}
+	
+	protected Bitmap doInBackground(Uri... uris) {
+		conectar();
+		Bitmap bitmap = null;
+		Uri uri = null;
+		if(uris.length > 0 && uris[0] != null){
+			uri = uris[0];
+		}
+		switch (request.getId()){
+			case ModuloImagen.RQS_BUSQUEDA_IMAGEN_UNICA:
+				bitmap = buscarImagen();
+				break;
+			case ModuloImagen.RQS_INSERTAR_IMAGEN:
+				insertarImagen(request.getInputStreamContext(uri));
+				break;
+		}
+		
+		desconectar();
+		return bitmap;
+	}
+	private boolean insertarImagen(InputStream is){
+		boolean guardo = false;
+		try {
+			guardo = ftp.storeFile(request.getPath(), is);
+			is.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		Log.v("Guardar",""+guardo);
+		return guardo;
+	}
+	private Bitmap buscarImagen(){
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		boolean busco = false;
+		Bitmap bitmap = null;
+		try {
+			busco = ftp.retrieveFile(request.getPath(),os);
+			if(busco){
+				os.close();
+				byte[] bytes = os.toByteArray();
+				bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Log.v("Obtener",""+busco);
+		return bitmap;
+	}
+	private boolean conectar(){
+		boolean conecto = false;
+		try {
+			ftp = new FTPClient();
+			ftp.connect("files.000webhost.com",21);
+			if(FTPReply.isPositiveCompletion(ftp.getReplyCode())){
+				conecto = ftp.login("mymuseum", "39623299Aa");
+				ftp.setFileType(FTP.BINARY_FILE_TYPE);
+				ftp.enterLocalPassiveMode();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return conecto;
+	}
+	private void desconectar(){
+		if(ftp != null && ftp.isConnected()){
+			try {
+				ftp.logout();
+				ftp.disconnect();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Override
+	protected void onPostExecute(Bitmap bitmap) {
+		super.onPostExecute(bitmap);
+		notificarModulo(bitmap,"Exito");
+	}
+	
+	private void notificarModulo(Bitmap g, String respuesta){
+		ModuloImagen.obtenerModulo().resultadoControlFTP(request,respuesta,g);
+	}
+}
